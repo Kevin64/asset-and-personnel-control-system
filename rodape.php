5bcbdf23f3f674f206225a7af48032a1ae518b63
Kevin Costa <kevinkentuxo@hotmail.com>
2021-06-05 16:06:49 -0300

    Fix
    - Updated version.

--- rodape.php
+++ rodape.php
@@ -1,7 +1,7 @@
 	<div id="rodape">
 		<font style="font-size: 10px;">
 		<b>Controle de Patrimônio e Docentes</b><br>
-		Versão: 2.2.0.2105<br>
+		Versão: 2.2.0.2106<br>
 		Sistema de desenvolvido pela Unidade de Tecnologia da Informação do CCSH<Br>
 		E-mail: lab74cufsm@gmail.com<br>
 		Telefone: 3220-9625<br>		
