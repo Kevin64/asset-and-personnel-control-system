32985ecad7f6f0adcb5530743550e74a067d3c75
Kevin Costa <kevinkentuxo@hotmail.com>
2021-11-16 16:34:44 -0300

    Bug fixes
    - Fixed a bug when deleting the bios.json and bios-checksum.txt files.
    - Added OS data when registering in maintenance mode.

--- forneceDadosBIOS.php
+++ forneceDadosBIOS.php
@@ -11,6 +11,12 @@ $biosChecksum = 'bios-checksum.txt';
 
 $query = mysql_query("select * from bios") or die ("Erro na query! ".mysql_error());
 $return_arr = array();
+
+if(file_exists($biosFile) || file_exists($biosChecksum)) {
+	unlink($biosFile);
+	unlink($biosChecksum);
+}
+
 while ($row = mysql_fetch_array($query, MYSQL_ASSOC)) {
 	$row_array['id'] = $row['id'];
 	$row_array['marca'] = $row['marca'];
@@ -19,18 +25,13 @@ while ($row = mysql_fetch_array($query, MYSQL_ASSOC)) {
 	$row_array['tipo'] = $row['tipo'];
 	array_push($return_arr,$row_array);
 
-	if(file_exists($biosFile) || file_exists($biosChecksum)) {
-		unlink($biosFile);
-		unlink($biosChecksum);
-	}
-
 	$fp = fopen($biosFile, 'w');
 	fwrite($fp, json_encode($return_arr));
 	$checksum = sha1(json_encode($return_arr));
 	$fp2 = fopen($biosChecksum, 'w');
 	fwrite($fp2, $checksum);
 	fclose($fp);
-	fclose($fp2);
+	fclose($fp2);	
 }
 
 ?>
