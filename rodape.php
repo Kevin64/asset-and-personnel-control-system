9178fe5d340194ce8be26b2e63df5f175be92ff2
Kevin Costa <kevinvtc@outlook.com>
2023-01-25 12:13:37 -0300

    Update rodape.php
    - Site version is now read from a text file

--- rodape.php
+++ rodape.php
@@ -1,7 +1,15 @@
+	<?php
+	if ($file = fopen("version.txt", "r")) {
+		while(!feof($file)) {
+			$line = fgets($file);
+		}
+		fclose($file);
+	}
+	?>
 	<div id="rodape">
 		<font style="font-size: 14px;">
 			<b>Sistema de controle de patrimônio e docentes do CCSH</b><br>
-			Versão: 3.2.1.2301<br>
+			Versão: <?php echo $line ?><br>
 			Sistema de desenvolvido pela Subdivisão de Tecnologia da Informação do CCSH<Br>
 			E-mail: ti.ccsh@ufsm.br<br>
 			Telefone: 3220-9625<br>
